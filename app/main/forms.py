22542aeb3e2dfbd9badbe349ce06afd09949e744
Akumucollins <akumucollins@gmail.com>
2020-09-30 19:24:05 +0300

    created a amain blueprint, forms and routes

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about youself.',validators = [Required()])
+    submit = SubmitField('Submit')
+    
+class PostForm(FlaskForm):
+    title = StringField('Blog Title')
+    topic = StringField('Blog Topic')
+    content = TextAreaField('Blog Content')
+    submit = SubmitField('Submit')
+    
