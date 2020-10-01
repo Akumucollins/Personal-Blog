2a237628ef7a40218518d6bb066d40dba18b07b6
Akumucollins <akumucollins@gmail.com>
2020-10-01 05:54:26 +0300

    created routes for comments , delete and update blog, delete comment

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
-from flask import render_template,redirect,url_for,abort,request,flash
+from flask import render_template, request, redirect, url_for, abort, flash
 from flask_login import login_required,current_user
 from . import main
-from .forms import *
-from ..models import *
+from ..models import User, Post, Comment
+from .forms import UpdateProfile, PostForm, CommentForm
 from .. import db, photos
 import markdown2
+from ..requests import get_quotes
 
 @main.route('/')
 def index():
@@ -76,6 +77,95 @@ def new_post():
         db.session.commit()
 
         title='New Blog Posted'
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.all'))
 
     return render_template('post.html',posts_form= form)
+
+@main.route('/Post/posts', methods=['GET', 'POST'])
+@login_required
+def posts():
+    posts = Post.query.all()
+    quote = get_quotes()
+    return render_template('posts.html', posts=posts, quote=quote)
+
+@main.route('/view/<int:id>', methods=['GET', 'POST'])
+@login_required
+def view(id):
+    post = Post.query.get_or_404(id)
+    post_comments = Comment.query.filter_by(post_id=id).all()
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        new_comment = Comment(post_id=id, comment=comment_form.comment.data, author=current_user)
+        new_comment.save_comment()
+    return render_template('post.html', post=post, post_comments=post_comments, comment_form=comment_form)
+
+@main.route('/post/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_post(id):
+    """
+    Delete a post from the database
+    """
+    post = Post.query.get_or_404(id)
+
+    if post.author != current_user:
+        abort(403)
+
+    post = Post.query.filter_by(id=id).first()    
+
+    db.session.delete(post)
+    db.session.commit()
+
+    flash('Your post has been deleted', 'successfully')
+    return redirect(url_for('main.posts'))
+
+@main.route('/Update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update_post(id):
+    """
+    Update or edit a post in the database
+    """
+    post = Post.query.get_or_404(id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.topic = form.topic.data
+        post.content = form.content.data
+        post.title =form.title.data
+
+        # Updated bloginstance
+        db.session.add(post)
+        db.session.commit()
+
+        flash('Your post has been Updated', 'successfully')
+        return redirect(url_for('main.posts'))
+
+    elif request.method == 'GET':
+       form.topic.data =  post.topic
+       form.content.data = post.content
+       form.title.data =  post.title
+        
+    return render_template('update_post.html', form=form)
+
+@main.route('/comment/<int:id>', methods=['GET', 'POST'])
+@login_required
+def comment(id):
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        new_comment = Comment(post_id=id, comment=comment.form.data, author=current_user)
+        new_comment.save_comment()
+    return render_template('post.html', comment_form=comment_form)  
+
+@main.route('/comment/delete/<int:post_id>' ,methods=['GET', 'POST'])
+@login_required
+def delete_comment(post_id):
+
+    post = Post.query.filter_by(id=post_id).first()
+    comment = Comment.query.filter_by(post_id=post_id).first()
+
+    db.session.delete(comment)
+    db.session.commit()
+
+    return redirect(url_for('main.post', comment=comment, post=post, post_id=post_id))      
+
